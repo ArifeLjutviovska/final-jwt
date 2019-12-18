d08bb349de163ecd674d38f0fb161cba482031c8
ArifeLjutviovska <arife.lyutviovska@hotmail.com>
2019-12-18 14:09:08 +0100

    initial commit

--- src/main/java/springbootjwtauthenticationpostgresql/springbootjwtauthenticationpostgresql/SpringBootJwtAuthenticationPostgresqlApplication.java
+++ src/main/java/springbootjwtauthenticationpostgresql/springbootjwtauthenticationpostgresql/SpringBootJwtAuthenticationPostgresqlApplication.java
@@ -45,9 +45,9 @@ public class SpringBootJwtAuthenticationPostgresqlApplication {
     private Connector httpToHttpsRedirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(8082);
+        connector.setPort(8080);
         connector.setSecure(false);
-        connector.setRedirectPort(8443);
+        connector.setRedirectPort(9443);
         return connector;
     }
 
