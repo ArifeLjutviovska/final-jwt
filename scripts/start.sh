bed7713e21ffd82d264c495a699a32aadde3e9a6
ArifeLjutviovska <arife.lyutviovska@hotmail.com>
2019-12-23 12:17:49 +0100

    initial commit

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd /home/ec2-user/login-register
-sudo /usr/bin/java -jar -Dserver.port=8443 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar target/spring-boot-jwt-authentication-postgresql-0.0.1-SNAPSHOT.jar
